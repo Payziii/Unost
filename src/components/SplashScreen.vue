625a7f1abc152c332148578364a3bd3b71f26619
Payziii <velmikhail123@gmail.com>
2025-10-16 12:43:52 +0500

    ээээээээээээээээээээээээээээээээээээээ

--- src/components/SplashScreen.vue
+++ src/components/SplashScreen.vue
@@ -18,7 +18,7 @@
   display: flex;
   justify-content: center;
   align-items: center;
-  background-color: var(--soft-white);
+  background-color: var(--yellow);
   z-index: 9999;
   transition: opacity 0.5s ease-out;
 }
