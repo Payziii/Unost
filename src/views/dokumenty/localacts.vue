d6e596faef60a4d1ac0c82329ae3cd2660518f9b
dedEfim01 <neyroefim@gmail.com>
2025-10-21 16:58:27 +0500

    я люблю сво хуй

--- src/views/dokumenty/localacts.vue
+++ src/views/dokumenty/localacts.vue
@@ -10,3 +10,21 @@ import Card from '@/components/pages/Card.vue';
 import TitledImage from '@/components/pages/TitledImage.vue';
 import Bold from '@/components/pages/Bold.vue';
 </script>
+
+<template>
+    <Page title="Профсоюз">
+        <Text align="justify">
+            <File file="Л1.pdf" icon>Положение о порядке и условиях приёма граждан для обучения в КШИ</File>
+            <File file="Л2.pdf" icon>Положение о кадетской школе интернат</File>
+            <File file="Л3.pdf" icon>Акт готовности ОО к 2025-2026 учебному году</File>
+            <File file="Л4.pdf" icon>Акт готовности образовательной организации к 2024-2025 году</File>
+            <File file="Л5.pdf" icon>Акт готовности образовательной организации к 2023-2024 году</File>
+            <File file="Л6.pdf" icon>АКТ готовности образовательной организации к 2022-2023 учебному году</File>
+            <File file="Л7.pdf" icon>Коллективный договор</File>
+            <File file="Л8.pdf" icon>Правила внутреннего трудового распорядка</File>
+            <File file="Л9.pdf" icon>Положение о формировании порядка</File>
+            <File file="Л10.pdf" icon>Договор на оказания услуг по представлению доступа к ЭБС</File>
+            <File file="Л10.pdf" icon>Оплата временной нетрудоспособности</File>
+        </Text>
+    </Page>
+</template>  
