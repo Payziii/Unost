cc6a1222131dd5dcc27c7b9eca516c28f065333f
SenkoBread07 <senkobread07@vk.com>
2025-10-21 11:09:09 +0500

    ССК и ещё пару изменений в страницах

--- src/views/struktura/ssc.vue
+++ src/views/struktura/ssc.vue
@@ -33,22 +33,68 @@ import Bold from '@/components/pages/Bold.vue';
     />
     </Card>
     </CardGrid>
+    <CardGrid maxCardsPerRow=3>
+    <Card>
+    <Text align="center">
+        <Bold>Волейбол</Bold><br>     
+        Кривоусова 53<br>
+        Понедельник: 18.00-19.00<br>
+        Среда: 18.00-19.00
+    </Text>
+    </Card>
+
+    <Card>
+        <Text align="center">
+        <Bold>Баскетбол</Bold><br>     
+        Кривоусова 53<br>
+        Вторник: 19.00-20.00<br>
+        Четверг: 19.00-20.00
+    </Text>
+    </Card>
+
+    <Card>
+        <Text align="center">
+        <Bold>Общая физическая подготовка</Bold><br>     
+        Лесная 1 (Тренажёрный зал)<br>
+        Вторник: 18.00-20.00<br>
+        Четверг: с 18.00-20.00
+    </Text>
+    </Card>
+
+    </CardGrid>
     <Text align="justify">
         <Highlight>Календарный план 2024-2025 (ОТСУТСТВУЕТ)</Highlight>
     </Text>
-
-    <CardGrid maxCardsPerRow=2>
-    <Card>
-    <Text align="justify">
-        <Bold>Баскетбол, воллейбол</Bold><br>
-        Понедельник: 18.00-20.00<br>
-        Среда: 18.00-20.00
-    </Text>
-    </Card>
-    </CardGrid>
     <File file="ПланРаботыССК.pdf" icon>План работы ССК</File>
     <File file="УставССК.pdf" icon>Устав Студенческого спортивного клуба</File>
     <File file="ПротоколОбщегоСобрания.pdf" icon>Протокол общего собрания ССК</File>
     <File file="Отчёт о деятельности  ССК 23-24 (2).docx" icon>Отчёт о деятельности  ССК за 2023-2024</File>
+    <File file="РасписаниеРаботССК.pdf" icon>Расписание Работы ССК</File>
+    <Text align="justify">
+        <Highlight>Наши новости (ОТСУТСТВУЕТ)</Highlight>
+    </Text>
+    <CardGrid maxCardsPerRow=3>
+    <Card>
+    <img 
+        src="/images/ssc/ssc1.jpeg" 
+        alt="сск" 
+        class="logo"
+    />
+    </Card>
+    <Card>
+    <img 
+        src="/images/ssc/ssc2.jpeg" 
+        alt="сск" 
+        class="logo"
+    />
+    </Card>
+    <Card>
+    <img 
+        src="/images/ssc/ssc3.jpeg" 
+        alt="сск" 
+        class="logo"
+    />
+    </Card>
+    </CardGrid>
     </Page>
 </template>
