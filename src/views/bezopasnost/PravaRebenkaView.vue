0f0849293ccf6358b1bf01cbd549bbdb9bb6b760
Payziii <velmikhail123@gmail.com>
2025-10-21 13:17:04 +0500

    Права детей ребенка

--- src/views/bezopasnost/PravaRebenkaView.vue
+++ src/views/bezopasnost/PravaRebenkaView.vue
@@ -4,6 +4,7 @@ import Title from '@/components/pages/Title.vue';
 import Text from '@/components/pages/Text.vue';
 import Bold from '@/components/pages/Bold.vue';
 import Link from '@/components/pages/Link.vue';
+import File from '@/components/pages/File.vue';
 </script>
 
 <template>
@@ -11,7 +12,12 @@ import Link from '@/components/pages/Link.vue';
     <Title>Защита прав ребёнка</Title>
 
     <Text align="justify">
-      
+      <File icon file="N3HN7nR3dDdi6rr9QSdsz2rbb.pdf">Правовое консультирование</File>
+      <File icon file="b642nfDkdezBdRSY8SnQfiazN.pdf">Права и обязанности детей и родителей</File>
+      <File icon file="R98Qhy8trStaERD8AnBND79eR.pdf">Памятка для подростка</File>
+      <File icon file="yh6T9Y2zBET2EnkFARYd2iYts.pdf">Памятка для подростка ты и закон</File>
+      <File icon file="ezSSzA9FAbhDnShDSFe7RS9iA.pdf">Конвенция о правах ребенка</File>
+      <File icon file="thabQbkGTkazRnhKdFTz9Gz43.pdf">Информация о территориальной комиссии по делам несовершеннолетних</File>
     </Text>
   </Page>
 </template>
