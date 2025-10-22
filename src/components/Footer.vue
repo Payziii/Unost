0bcf19a4c22b85af22cc34968a61538330d0d413
Payziii <velmikhail123@gmail.com>
2025-10-22 10:15:31 +0500

    Добавил Ефима

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -16,6 +16,7 @@ import { ref, onMounted } from 'vue';
             <a href="https://github.com/Ab4kshin" target="_blank">Ab4kshin</a>,
             <a href="https://github.com/LegendaUral" target="_blank">LegendaUral</a>,
             <a href="https://github.com/SenkoBread07" target="_blank">SenkoBread07</a>
+            <a href="https://github.com/dedEfim01" target="_blank">dedEfim01</a>
           </h3>
         </div>
       </div>
